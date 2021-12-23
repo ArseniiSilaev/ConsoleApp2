094caa0080a2d25faded706f363d6375a5b96784
Senya <Senya@DESKTOP-3VEI78Q>
2021-12-23 11:27:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int rows = 3;
+            int numberOfFullRows = pictures / rows;
+            int remainingOfPictures = pictures % rows;
+            Console.WriteLine("Можно будет вывести " + numberOfFullRows +
+                " заполненных рядов и " + remainingOfPictures +
+                " картинок будет сверх меры");
+        }
+    }
+}
